554133c5d57594b762855f0f3e38a3504bf6d039
TheLucasMoore <lucasedwardmoore@gmail.com>
2016-05-02 18:54:43 -0500

    Done.

--- code.js
+++ code.js
@@ -1,1 +1,17 @@
 'use strict';
+
+function favIceCream(ice) {
+  return "I love " + ice;
+}
+
+function shouting(message) {
+  return message.toUpperCase();
+};
+
+function roundDown(number) {
+  return Math.floor(number);
+}
+
+function theTruth() {
+  return true;
+}
